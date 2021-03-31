b2f56b811c2e9010e3e46ce8838091b79fedbc30
Ali N <namnamir@users.noreply.github.com>
2021-03-31 08:58:24 +0200

    Create api.py

---
+++ api.py
@@ -0,0 +1,120 @@
+apis = {
+    'PhishTank': [
+        {
+            'name': 'PT_01',
+            'url': 'http://checkurl.phishtank.com/checkurl/',
+            'api_key': ''
+        }
+    ],
+    'GoogleSafeBrowsing': [
+        {
+            'name': 'GSB_01',
+            'url': 'https://safebrowsing.googleapis.com/v4/threatMatches:find',
+            'api_key': '',
+            'client': {
+                'clientId': 'URL Checker',
+                'clientVersion': '1.0'
+            }
+        }
+    ],
+    'URLScan': [
+        {
+            'name': 'US_01',
+            'url': 'https://urlscan.io/api/v1/scan/',
+            'api_key': ''
+        }
+    ],
+    'OXT': [
+        {
+            'name': 'OXT_01',
+            'url': 'https://otx.alienvault.com/api/v1/',
+            'api_key': ''
+        }
+    ],
+    # https://developer.greynoise.io/docs/authentication
+    'GreyNoise': [
+        {
+            'name': 'GN_01',
+            'url': 'https://api.greynoise.io/v2/noise/context/',
+            'api_key': ''
+        }
+    ],
+    # https://docs.securitytrails.com/docs
+    'SecurityTrials': [
+        {
+            'name': 'ST_01',
+            'url': 'https://api.securitytrails.com/v1/domain/',
+            'api_key': ''
+        }
+    ],
+    # https://www.mcafee.com/enterprise/en-us/solutions/mvision/developer-portal.html
+    'McAfeeMVision': [
+        {
+            'name': 'MAMV_01',
+            'url': 'https://api.securitytrails.com/v1/domain/',
+            'api_key': ''
+        }
+    ],
+    # https://botscout.com/api.htm
+    'BotScout': [
+        {
+            'name': 'BS_01',
+            'url': 'http://botscout.com/test/',
+            'api_key': ''
+        }
+    ],
+    # https://threatintelligenceplatform.com/threat-intelligence-api-docs/domain-malware-check-api
+    'ThreatIntelligencePlatform': [
+        {
+            'name': 'TIP_01',
+            'url': 'https://api.threatintelligenceplatform.com/v1/',
+            'api_key': ''
+        }
+    ],
+    'RiskIQ': [
+        {
+            'name': 'RIQ_01',
+            'url': 'https://otx.alienvault.com/api/v1/',
+            'user': '',
+            'api_key': ''
+        }
+    ],
+    # https://www.brightcloud.com/web-service/api-documentation
+    'BrightCloud': [
+        {
+            'name': 'BC_01',
+            'url': 'https://otx.alienvault.com/api/v1/',
+            'user': '',
+            'api_key': ''
+        }
+    ],
+    # https://docs.abuseipdb.com/
+    'AbouseIPDB': [
+        {
+            'name': 'AIP_01',
+            'url': 'https://docs.abuseipdb.com/',
+            'api_key': ''
+        }
+    ],
+    # https://www.hybrid-analysis.com/docs/api/v2#/Quick%20Scan/post_quick_scan_url
+    'HybridAnalysis': [
+        {
+            'name': 'HA_01',
+            'url': 'https://docs.abuseipdb.com/',
+            'api_key': ''
+        }
+    ],
+
+
+
+    ##
+    #
+    'AWIS': [
+        {
+            'name': 'RIQ_01',
+            'url': 'https://otx.alienvault.com/api/v1/',
+            'user': '',
+            'api_key': ''
+        }
+    ]
+}
